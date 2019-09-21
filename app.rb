5f222339cab0931092f1a602b38c9c86759224a9
Alastair McGilloway <alastairmcgilloway@hotmail.com>
2019-09-21 11:00:46 +0100

    add home page and navbar

---
+++ app.rb
@@ -0,0 +1,9 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+# require_relative('controllers/properties_controller')
+# require_relative('controllers/tenants_controller')
+# require_relative('controllers/rentals_controller')
+
+get '/' do
+  erb( :index )
+end
